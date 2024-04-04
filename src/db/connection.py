d572f35bccb25a436525527790e7fe026cace4c9
Rasmus Lauritsen <rasmus.lauritsen.dev@outlook.com>
2024-04-04 17:03:50 +0200

    Refactor cursor object retrieval

--- src/db/connection.py
+++ src/db/connection.py
@@ -4,25 +4,16 @@ from .singleton_meta import SingletonMeta
 
 
 class Connection(metaclass=SingletonMeta):
-    def __init__(self, db_name=None):
+    def __init__(self, db_name):
         self.db_name = db_name
-        self.connection = None
-        self.cursor = None
-        if self.db_name:
-            self.connect()
+        self.connection = sqlite3.connect(self.db_name, check_same_thread=False)
 
-    def connect(self, db_name=None) -> None:
-        if db_name:
-            self.db_name = db_name
-        if self.db_name and not self.connection:
-            self.connection = sqlite3.connect(self.db_name)
-            self.cursor = self.connection.cursor()
+    def cursor(self):
+        return self.connection.cursor()
 
     def disconnect(self) -> None:
         if self.connection:
             self.connection.close()
-            self.connection = None
-            self.cursor = None
 
     def is_connected(self) -> bool:
         return self.connection is not None
